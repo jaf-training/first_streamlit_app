f79fd45a7d87a92f68535c7a098fcca28897c424
jaf-training <107966497+jaf-training@users.noreply.github.com>
2022-06-22 10:02:00 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('Snowflake Training')
 streamlit.header('June 2022')
 
 streamlit.text('Hello World!')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
