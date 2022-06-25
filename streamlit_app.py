cc2f8acfb81cd88cd3790f03d578c32deac9ec95
jaf-training <107966497+jaf-training@users.noreply.github.com>
2022-06-25 16:59:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,14 @@ import snowflake.connector
 
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+  
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    
+    return fruityvice_normalized
+  
+
 
 streamlit.title('Snowflake Training')
 streamlit.header('June 2022')
@@ -28,22 +36,25 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # normalize json response
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # display in df
+    fruityvice_normalized = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
 
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+    
+if streamlit.button("Get Fruit Load List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  
 streamlit.dataframe(my_data_rows)
 
+
+streamlit.stop()
 my_cur.execute("insert into fruit_load_list values ('from streamlist')")
