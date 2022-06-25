3afb939156d1d8c6dba9a96292df5c3cd86e905e
jaf-training <107966497+jaf-training@users.noreply.github.com>
2022-06-25 15:48:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('API call')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# normalize json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display in df
+streamlit.dataframe(fruityvice_normalized)
